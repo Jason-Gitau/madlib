8eaa81158a8a4332dbe68ba6f8e03dedfe074733
Jason-Gitau <92223939+Jason-Gitau@users.noreply.github.com>
2023-01-03 15:54:20 +0300

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,6 @@
+adj=input('adjective: ')
+verb1=input('verb: ')
+famous_person=input('famous person: ')
+madlib=f"computer programming is so {adj}!it makes me feel excited all the time \
+because i love to {verb1}.stay hydrated like you are {famous_person}"
+print (madlib)
